2d3c96ac2a9ef46a5b831dbbedd32b6f1eb99e5f
Jelle den Butter <jelle@frmwrk.nl>
2021-04-12 21:30:59 +0200

    Update texts and logo

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home({ markdownFile }) {
                     <Card style={{textAlign: 'center'}}>
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/bevers_RGB.png" />
                         <Card.Title>Bevers</Card.Title>
-                        <Card.Subtitle>5 t/n 7 jaar</Card.Subtitle>
+                        <Card.Subtitle>5 t/m 7 jaar</Card.Subtitle>
                         <Card.Body>De jongste groep jongens en meiden bij Scouting A15</Card.Body>
                     </Card>
                 </Col>
@@ -59,7 +59,7 @@ export default function Home({ markdownFile }) {
                     <Card style={{textAlign: 'center'}}>
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/welpen_RGB.png" />
                         <Card.Title>Welpen</Card.Title>
-                        <Card.Subtitle>7 t/n 11 jaar</Card.Subtitle>
+                        <Card.Subtitle>7 t/m 11 jaar</Card.Subtitle>
                         <Card.Body>Deze jongens zijn niet te stoppen in Jungle Book</Card.Body>
                     </Card>
                 </Col>
@@ -76,7 +76,7 @@ export default function Home({ markdownFile }) {
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/scouts_RGB.png" />
                         <Card.Title>Verkenners</Card.Title>
                         <Card.Subtitle>11 t/m 15 jaar</Card.Subtitle>
-                        <Card.Body></Card.Body>
+                        <Card.Body>Jongens vol met energie. Niet te stoppen.</Card.Body>
                     </Card>
                 </Col>
                 <Col lg="3">
@@ -84,7 +84,7 @@ export default function Home({ markdownFile }) {
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/scouts_RGB.png" />
                         <Card.Title>Meidenscouts</Card.Title>
                         <Card.Subtitle>11 t/m 15 jaar</Card.Subtitle>
-                        <Card.Body>De jongste groep jongens en meiden bij Scouting A15</Card.Body>
+                        <Card.Body>Deze meiden zijn altijd in voor gezelligheid.</Card.Body>
                     </Card>
                 </Col>
                 <Col lg="3">
@@ -92,7 +92,7 @@ export default function Home({ markdownFile }) {
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/scouts_RGB.png" />
                         <Card.Title>Blauwe vogels</Card.Title>
                         <Card.Subtitle>Verstandelijk beperkt 18+</Card.Subtitle>
-                        <Card.Body>De jongste groep jongens en meiden bij Scouting A15</Card.Body>
+                        <Card.Body>Jongens en meiden die overal voor in zijn.</Card.Body>
                     </Card>
                 </Col>
                 <Col lg="3">
@@ -100,7 +100,7 @@ export default function Home({ markdownFile }) {
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/explorers_RGB.png" />
                         <Card.Title>Explorers</Card.Title>
                         <Card.Subtitle>15-18</Card.Subtitle>
-                        <Card.Body>Jongens en meiden die zelf de uitdagen opzoeken</Card.Body>
+                        <Card.Body>Jongens en meiden die zelf de uitdagen opzoeken.</Card.Body>
                     </Card>
                 </Col>
                 <Col lg="3">
@@ -108,15 +108,15 @@ export default function Home({ markdownFile }) {
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/roverscouts_RGB.png" />
                         <Card.Title>Roverscouts</Card.Title>
                         <Card.Subtitle>18-21</Card.Subtitle>
-                        <Card.Body>De jongste groep jongens en meiden bij Scouting A15</Card.Body>
+                        <Card.Body>Jongens en meiden die scout zijn naar een nieuw niveau brengen.</Card.Body>
                     </Card>
                 </Col>
                 <Col lg="3">
                     <Card style={{textAlign: 'center'}}>
                         <Card.Img style={{width: '50%', margin: 'auto', marginBottom: '2em'}} src="/sn/roverscouts_RGB.png" />
                         <Card.Title>Stam Aflag 26</Card.Title>
-                        <Card.Subtitle>18+</Card.Subtitle>
-                        <Card.Body>Alle leiding, vrijwilligers en gezellig mensen die soms toffe activiteiten ondernemen met elkaar</Card.Body>
+                        <Card.Subtitle>21+</Card.Subtitle>
+                        <Card.Body>Alle leiding, vrijwilligers en gezellig mensen die soms toffe activiteiten ondernemen met elkaar.</Card.Body>
                     </Card>
                 </Col>
             </Row>
